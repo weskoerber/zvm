07fea21b303c50c8b49528072b0d33a5e3fd2c48
Wesley Koerber <wes@weskoerber.com>
2024-08-03 14:05:38 -0500

    feat: zsh shell completions (#9)
    * feat: zsh shell completions
    
    * chore: completions for rm command
    
    * feat: bash completion script
    
    * chore: add missing options for zsh completions
    
    * chore: improve bash completions
    
    * chore: rename completion scripts

---
+++ completions/zvm-completions.bash
@@ -0,0 +1,52 @@
+_zvm_completions()
+{
+    local cur prev commands global_options list_options
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    commands="help install list prune uninstall use"
+    global_options="-h --help -t --target -v -vv --verbose"
+    list_options="-i --installed"
+    install_options="-u --use"
+    uninstall_options="-f --force"
+
+    zvm_local_versions="$(zvm list --installed)"
+
+    # Completing the global options for `zvm`
+    if [[ ${COMP_CWORD} -eq 1 ]] && [[ ${cur} == -* ]]; then
+        COMPREPLY=( $(compgen -W "${global_options}" -- ${cur}) )
+        return 0
+    fi
+
+    # Completing the commands
+    case ${COMP_CWORD} in
+        0)
+            # should never happen?
+            return 0
+            ;;
+        1)
+            COMPREPLY=( $(compgen -W "${commands}" -- ${cur}) )
+            return 0
+            ;;
+    esac
+
+    local cmp_list
+    case "${COMP_WORDS[1]}" in
+        ls | list)
+            [[ "${cur}" == -* ]] && cmp_list="${list_options}"
+            ;;
+        i | install)
+            [[ "${cur}" == -* ]] && cmp_list="${install_options}" || cmp_list="$(zvm list)"
+            ;;
+        rm | uninstall)
+            [[ "${cur}" == -* ]] && cmp_list="${uninstall_options}" || cmp_list="${zvm_local_versions}"
+            ;;
+        use)
+            cmp_list="${zvm_local_versions}"
+            ;;
+    esac
+    COMPREPLY=( $(compgen -W "${cmp_list}" -- ${cur}) )
+}
+
+complete -F _zvm_completions zvm
---
+++ completions/zvm-completions.zsh
@@ -0,0 +1,79 @@
+#compdef zvm
+
+if ! command -v zvm &> /dev/null; then
+    return
+fi
+
+local -a zvm_commands
+local -a zvm_aliases
+local -a zvm_global_options
+local -a zvm_list_options
+local -a zvm_install_options
+local -a zvm_uninstall_options
+
+local -a zvm_local_versions
+local -a zvm_remote_versions
+
+zvm_commands=(
+    'list:List versions'
+    'use:Use installed version'
+    'install:Install version'
+    'uninstall:Uninstall version'
+    'prune:Uninstall all unused versions'
+    'help:Show help'
+)
+
+zvm_aliases=(
+    'ls:List versions'
+    'i:Install version'
+    'rm:Uninstall version'
+    'h:Show help'
+)
+
+zvm_global_options=(
+    '(-h --help)'{-h,--help}'[Show help]'
+    '(-t --target)'{-t,--target}'[Specify target]'
+    '(-v)-v[Print version and exit]'
+    '(--verbose)--verbose[Show verbose output]'
+    '(-vv)-vv[Enable more verbose output]'
+)
+
+zvm_list_options=(
+    '(-i --installed)'{-i,--installed}'[List installed versions only]'
+)
+
+zvm_install_options=(
+    '(-u,--use)'{-u,--use}'[Make version active after installing]'
+)
+
+zvm_uninstall_options=(
+    '(-f,--force)'{-f,--force}'[Forcefully uninstall, even if version is active]'
+)
+
+_arguments -C \
+    $zvm_global_options \
+    "1: :{_describe 'command' zvm_commands -- zvm_aliases}" \
+    "*::arg:->args"
+
+case $line[1] in
+    ls | list)
+        _arguments $zvm_list_options
+        ;;
+    i | install)
+        zvm_remote_versions=(${(f)"$(zvm list)"})
+        _arguments \
+            $zvm_install_options \
+            "*:: :{_describe 'command' zvm_remote_versions}"
+        ;;
+    use)
+        zvm_local_versions=(${(f)"$(zvm list --installed)"})
+        _arguments \
+            "1: :{_describe 'command' zvm_local_versions}"
+        ;;
+    rm | uninstall)
+        zvm_local_versions=(${(f)"$(zvm list --installed)"})
+        _arguments \
+            $zvm_uninstall_options \
+            "1: :{_describe 'command' zvm_local_versions}"
+        ;;
+esac
